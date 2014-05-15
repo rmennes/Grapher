041f647afaf204f92e43014193068012f1c5a224
Ruben Mennes <mennes.rubne@gmail.com>
2014-05-15 19:05:11 +0200

    Before merging

--- src/Grapher/ParserFactory.java
+++ src/Grapher/ParserFactory.java
@@ -5,7 +5,7 @@ import java.io.FileNotFoundException;
 import net.minidev.json.parser.ParseException;
 
 public class ParserFactory {
-	
+
 	public static Parser getParser(String type, String file) throws FileNotFoundException, ParseException{
 		String typeToCheck = type.toLowerCase();
 		if(typeToCheck.equals("throughput")) return new Throughput(file);
@@ -13,6 +13,7 @@ public class ParserFactory {
 		if(typeToCheck.equals("packet")) return new Packets(file);
 		if(typeToCheck.equals("packetloss")) return new PacketLoss(file);
 		if(typeToCheck.equals("lost")) return new Lost(file);
+		if(typeToCheck.equals("speed")) return new Speed(file);
 		return null;
 	}
 
